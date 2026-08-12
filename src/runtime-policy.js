861e3e0d9050626b245a39acbd3823c02e8b4011
annon <michi.unknown.zwei@gmail.com>
2026-08-12 15:28:10 +0900

    Remove unused response-tool runtime policy module

--- src/runtime-policy.js
+++
@@ -1,8 +0,0 @@
-export function evaluateAssistantRuntimeDispatchConstraint(){
-  return {
-    blocked:false,
-    actor:'relay_transport',
-    scope:'external_generation_dispatch_only',
-    code:'none'
-  };
-}
